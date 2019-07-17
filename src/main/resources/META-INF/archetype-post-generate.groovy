1f58288c833bc46f2d02ae8ac515bf1e7b2ccabe
Marcus Sorensen <marcus_sorensen@apple.com>
2019-07-17 15:13:20 -0600

    Fix typo in archetype-post-generate.groovy

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -3,6 +3,6 @@ def executablePaths = ["buildrpm.sh"]
 
 executablePaths.each {
     def file = new File( request.getOutputDirectory(), request.getArtifactId() + File.separator + "${it}" );
-    println "Setting " + file.getAbsolutePath() + "to executable"
+    println "Setting " + file.getAbsolutePath() + " to executable"
     file.setExecutable(true, false);
 }
